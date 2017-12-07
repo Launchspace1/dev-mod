2782ac1bc8d2988a0e5e61b397f3c4fdd857dc61
Vaishali <vaishalic@launchspace.academy>
2017-12-07 22:20:43 +1300

    adding file with appjs copy

--- test/test.js
+++ test/test.js
@@ -1,11 +1,8 @@
 var request = require('supertest');
 var app = require('../app.js');
-
+ 
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    superagent
-      .get('http://localhost:4300/customers')
-      .end(function(res){
-        expect(res.status).to.equal(200);
+    request(app).get('/').expect('hello world', done);
   });
 });
